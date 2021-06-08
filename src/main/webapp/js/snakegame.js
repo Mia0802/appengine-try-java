c041957f81837bbd59a32b1b4735e73221957859
Mia0802 <69360911+Mia0802@users.noreply.github.com>
2021-06-08 20:46:41 +0800

    Update snakegame.js

--- src/main/webapp/js/snakegame.js
+++ src/main/webapp/js/snakegame.js
@@ -46,8 +46,8 @@ function food() {
     this.x = 0, this.y = 0, this.w = 10, this.h = 10, this.color = foodcolor(), this.renew = function() {
         this.x = Math.floor(Math.random() * (w - 20) + 10);
         this.y = Math.floor(Math.random() * (w - 20) + 10);
-        this.color = foodcolor();
-        this.put = (() => {
+        this.color = foodcolor()
+    }, this.renew(), this.put = (() => {
             canvas_context.fillStyle = this.color;
             canvas_context.fillRect{this.x, this.y, this.w, this.h};
             
